f5c35f16abc2b3acf55d1669c3e6b52c006b9ebe
saachi-tiger <saachi.mohanty@tigeranalytics.com>
2024-05-30 00:21:50 +0530

    Change code file

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -35,7 +35,7 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
-
+fetch_housing_data()
 housing = load_housing_data()
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
