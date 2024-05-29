8446906edc705ff5bd89c2853038502c6bf73b2d
saachi-tiger <saachi.mohanty@tigeranalytics.com>
2024-05-30 00:22:40 +0530

    Change code file

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -35,6 +35,7 @@ def load_housing_data(housing_path=HOUSING_PATH):
     csv_path = os.path.join(housing_path, "housing.csv")
     return pd.read_csv(csv_path)
 
+
 fetch_housing_data()
 housing = load_housing_data()
 
