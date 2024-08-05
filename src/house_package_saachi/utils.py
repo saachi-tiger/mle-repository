fb7d2c49484690c95b3c0d0f1271642afd102658
saachi-tiger <saachi.mohanty@tigeranalytics.com>
2024-08-05 18:44:55 +0530

    Updated refactor code

--- src/house_package_saachi/utils.py
+++ src/house_package_saachi/utils.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -10,3 +11,19 @@ def stratified_split(df, strat_col, test_size=0.2, random_state=42):
         strat_train_set = df.loc[train_index]
         strat_test_set = df.loc[test_index]
     return strat_train_set, strat_test_set
+=======
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedShuffleSplit
+
+
+def stratified_split(df, strat_col, test_size=0.2, random_state=42):
+    split = StratifiedShuffleSplit(
+        n_splits=1, test_size=test_size, random_state=random_state
+    )
+    for train_index, test_index in split.split(df, df[strat_col]):
+        strat_train_set = df.loc[train_index]
+        strat_test_set = df.loc[test_index]
+    return strat_train_set, strat_test_set
+
+>>>>>>> 4720c7f (Updated refactor code)
